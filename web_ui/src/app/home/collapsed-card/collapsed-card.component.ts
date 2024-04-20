5c82a4c0daf4e35a8d7e5bb4a18ca556514a84a6
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-04-20 18:26:34 +0200

    Updated CollapsedCard component style

--- web_ui/src/app/home/collapsed-card/collapsed-card.component.ts
+++ web_ui/src/app/home/collapsed-card/collapsed-card.component.ts
@@ -11,16 +11,15 @@ import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CollapsedCardComponent {
   isCollapsed: boolean = true;
-  isActive: boolean = !this.isCollapsed;
-  @Input() index: number | undefined;
-  @Input() question: string | undefined;
-  @Input() answer: string | undefined;
+  @Input() index: number = 0;
+  @Input() question: string = 'Placeholder question';
+  @Input() answer: string =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos maxime ad cum cumque quibusdam. Recusandae dolor odit facere iste optio deserunt, eveniet natus tempore doloribus, eligendi, unde commodi? Dignissimos! Ex numquam nemo repellendus voluptas ea optio id in? Repellat cum hic id qui magni labore necessitatibus quos cumque dignissimos, explicabo et quae dolorum. Nobis accusamus harum asperiores aliquam delectus!';
 
   rotated: boolean = false;
 
   toggleAndRotate() {
     this.isCollapsed = !this.isCollapsed;
-    this.isActive = !this.isActive;
     this.rotated = !this.rotated;
   }
 }
