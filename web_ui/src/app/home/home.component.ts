6ea469de2ceefc8c33defbae004746822f3708d9
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-04-23 14:41:45 +0200

    Added components to the home page

--- web_ui/src/app/home/home.component.ts
+++ web_ui/src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CollapsedCardComponent } from './collapsed-card/collapsed-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent, CollapsedCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  loggedIn: boolean = false;
+  numberOfFaqs: number = 3;
 
+  counter(i: number) {
+    return new Array(i);
+  }
 }
