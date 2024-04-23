c2188df1c1b00d41373512e130c29fba826559ac
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-04-23 20:02:30 +0200

    Added toggle button to user settings and change icons

--- web_ui/src/app/user-navbar/user-navbar.component.ts
+++ web_ui/src/app/user-navbar/user-navbar.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-user-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './user-navbar.component.html',
-  styleUrl: './user-navbar.component.css'
+  styleUrl: './user-navbar.component.css',
 })
 export class UserNavbarComponent {
+  settings = [
+    {
+      name: 'Lingua',
+      icon_loc: '/assets/icons/ic-language.svg',
+      value: 'Spanish',
+      type: 'select',
+    },
+    {
+      name: 'Tema scuro',
+      icon_loc: '/assets/icons/moon.svg',
+      value: true,
+      type: 'toggle',
+    },
+    {
+      name: 'Notifiche',
+      icon_loc: '/assets/icons/notification.svg',
+      value: true,
+      type: 'toggle',
+    },
+    {
+      name: 'Share',
+      icon_loc: '/assets/icons/ic-share.svg',
+      value: true,
+      type: 'toggle',
+    },
+  ];
 
+  supportedLanguages: string[] = ['Italiano', 'English', 'Spanish', 'French'];
+
+  selected_options = [];
+
+  @ViewChild('language') language!: ElementRef;
+
+  onSelected(): void {
+    this.settings[0].value = this.language.nativeElement.value;
+  }
+
+  onToggle(index: number): void {
+    this.settings[index].value = !this.settings[index].value;
+  }
 }
