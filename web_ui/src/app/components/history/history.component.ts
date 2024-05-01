d8e3cbaffd8837e2c0da2782a52670bc17557250
FedericoGuidazzi <guidazzi.federico@gmail.com>
2024-05-01 20:01:52 +0200

    add dashboard, history and charts

--- web_ui/src/app/components/history/history.component.ts
+++ web_ui/src/app/components/history/history.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatInputModule} from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+
+export interface TransactionData {
+  id: string;
+  date: string;
+  type: string;
+  symbol: string;
+  quantity: number;
+  price: number;
+}
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatTableModule, MatSortModule, MatPaginatorModule, MatInputModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
 export class HistoryComponent implements OnInit{
-  transactions: any[] = []; // Assuming transaction data structure
-  
-  ngOnInit(): void {
-    // Fetch transactions data from API or service
-    this.getTransactions();
+
+  @Input() transactions: any[] = []; // Assuming transaction data structure
+  displayedColumns: string[] = ['date', 'type', 'symbol', 'quantity', 'price'];
+
+  constructor() {
   }
 
-  getTransactions() {
-    // Call API or service to get transaction data
-    // Assuming transactions is an array of objects with date, type, symbol, quantity, and price properties
-    this.transactions = [
-      { date: new Date(), type: 'Compra', symbol: 'AAPL', quantity: 10, price: 150 },
-      { date: new Date(), type: 'Vendita', symbol: 'GOOGL', quantity: 5, price: 250 },
-      // Add more transaction data as needed
-    ];
+  ngOnInit(): void {
+
   }
+
+  uploadData() {
+    //todo
+    throw new Error('Method not implemented.');
+  }
+
 }
