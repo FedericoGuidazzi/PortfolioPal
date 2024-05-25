c82514f5189fe7a849740ad7c5d5285144d42053
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-05-25 12:43:45 +0200

    chore: Add RabbitMQ configuration and services for user privacy updates

---
+++ user/src/main/java/com/example/user/configs/RabbitMqConfig.java
@@ -0,0 +1,14 @@
+package com.example.user.configs;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMqConfig {
+    @Bean
+    public Queue privacyUpdateQueue() {
+        return new Queue("privacyUpdates", true);
+    }
+}
---
+++ user/src/main/java/com/example/user/services/RabbitMqFirestoreReciver.java
@@ -0,0 +1,39 @@
+package com.example.user.services;
+
+import com.example.user.customExceptions.UserNotFoundException;
+import com.example.user.models.User;
+import com.example.user.models.bin.PutUserPrivacyBin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@EnableRabbit
+public class RabbitMqFirestoreReciver {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    FirebaseUserServiceImpl userService;
+
+    @RabbitListener(queues = "privacyUpdates")
+    public void updatePrivacy(String msg) {
+        System.out.println("\n\n[R] Received < " + msg + " >");
+
+        try {
+            PutUserPrivacyBin privacyBin = objectMapper.readValue(msg, PutUserPrivacyBin.class);
+
+            Optional<User> user = Optional.ofNullable(this.userService.getUser(privacyBin.getUserID()));
+            if (user.isPresent()) {
+                user.get().setSharePortfolio(privacyBin.isSharePortfolio());
+                this.userService.setUser(privacyBin.getUserID(), user.get());
+            } else {
+                throw new UserNotFoundException("User not found with id: " + privacyBin.getUserID());
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
