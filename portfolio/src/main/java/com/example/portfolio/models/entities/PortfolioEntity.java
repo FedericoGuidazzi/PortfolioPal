08bf08492151e4fa715a9ed5d5f533cb8dc357e1
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-05-25 12:55:25 +0200

    chore: Add Portfolio model and repository

---
+++ portfolio/src/main/java/com/example/portfolio/models/entities/PortfolioEntity.java
@@ -0,0 +1,30 @@
+package com.example.portfolio.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Entity
+@Table(name = "portfolios")
+public class PortfolioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private boolean sharePortfolio;
+}
