ac508411052f47953eb71bd4405a2c3acf14b574
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-06-20 12:15:57 +0200

    chore: Refactor CsvPortfolioReader to use Transaction class instead of TransactionEntity

--- transaction/src/main/java/com/example/transaction/models/CsvPortfolioReader.java
+++ transaction/src/main/java/com/example/transaction/models/CsvPortfolioReader.java
@@ -17,24 +17,24 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import com.example.transaction.custom_exceptions.CustomException;
-import com.example.transaction.models.entities.TransactionEntity;
 import com.example.transaction.models.enums.TransactionType;
 
 public class CsvPortfolioReader {
 
-    public static List<TransactionEntity> readCsvFile(InputStream inputStream) throws CustomException {
-        List<TransactionEntity> portfolioEntries = new ArrayList<>();
+    public static List<Transaction> readCsvFile(InputStream inputStream) throws CustomException {
+        List<Transaction> portfolioEntries = new ArrayList<>();
 
         try (
                 Reader reader = new InputStreamReader(inputStream);
                 CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
                         .withHeader("date", "type", "amount", "symbolId", "price", "portfolioId", "currency")
                         .withIgnoreHeaderCase()
-                        .withTrim());) {
+                        .withTrim()
+                        .withFirstRecordAsHeader());) {
             for (CSVRecord csvRecord : csvParser) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd").withLocale(Locale.ITALY);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd").withLocale(Locale.ENGLISH);
                 LocalDate date = LocalDate.parse(csvRecord.get("date"), formatter);
-                TransactionEntity entry = TransactionEntity.builder()
+                Transaction entry = Transaction.builder()
                         .type(Optional.ofNullable(
                                 TransactionType.fromValue(csvRecord.get("type")))
                                 .orElseThrow(() -> new CustomException("Invalid transaction type")))
