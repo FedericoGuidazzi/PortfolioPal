44e9e15631b183685b6efd9f88ddf00124b6b423
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-07-05 21:49:52 +0200

    chore: Add FireBaseTokenFilter to API Gateway routes

--- api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.api_gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,9 +8,14 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import com.example.api_gateway.filters.FireBaseTokenFilter;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
+
+	@Autowired
+	private FireBaseTokenFilter auTokenFilter;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
@@ -19,10 +25,12 @@ public class ApiGatewayApplication {
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/user/**")
-						.filters(f -> f.rewritePath("/user/(?<segment>.*)", "/api/v1/user/${segment}"))
+						.filters(f -> f.rewritePath("/user/(?<segment>.*)", "/api/v1/user/${segment}")
+								.filter(auTokenFilter))
 						.uri("lb://user"))
 				.route(r -> r.path("/transaction/**")
-						.filters(f -> f.rewritePath("/transaction/(?<segment>.*)", "/api/v1/transaction/${segment}"))
+						.filters(f -> f.rewritePath("/transaction/(?<segment>.*)", "/api/v1/transaction/${segment}")
+								.filter(auTokenFilter))
 						.uri("lb://transaction"))
 				.build();
 	}
---
+++ api-gateway/src/main/java/com/example/api_gateway/filters/FireBaseTokenFilter.java
@@ -0,0 +1,52 @@
+package com.example.api_gateway.filters;
+
+import java.util.Optional;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class FireBaseTokenFilter implements GatewayFilter {
+    /**
+     * Authenticating user via fireBase authorizer verify fireBase token and extract
+     * Uid and Email from token
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = "";
+        ServerHttpRequest request = exchange.getRequest();
+        if (!request.getHeaders().containsKey("Authorization")) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing token!");
+        }
+        token = Optional.ofNullable(request.getHeaders().get("Authorization"))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing token!")).toString()
+                .split(" ")[1];
+
+        FirebaseToken decodedToken;
+        try {
+            // verifies token to firebase server
+            decodedToken = Optional.ofNullable(FirebaseAuth.getInstance().verifyIdToken(token))
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token!"));
+        } catch (FirebaseAuthException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Error! " + e.toString());
+        }
+
+        // Extract Uid and Email
+        String uid = decodedToken.getUid();
+
+        exchange.getAttributes().put("uid", uid);
+        
+        return chain.filter(exchange);
+    }
+}
