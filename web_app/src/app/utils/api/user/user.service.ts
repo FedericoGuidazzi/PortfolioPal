1c0c01d6b198f69e6b66449983b0f2473530e933
Grandi Luca <73893837+Grandi-Luca@users.noreply.github.com>
2024-07-17 14:49:23 +0200

    chore: Add UserService for handling user-related API calls

---
+++ web_app/src/app/utils/api/user/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userUrl = `${environment.backendUrl}/user`;
+
+  constructor(private http: HttpClient) {}
+
+  getUser(): Observable<any> {
+    return this.http.get(this.userUrl + '/get');
+  }
+
+  getUserById(id: String): Observable<any> {
+    return this.http.get(this.userUrl + '/get/' + id);
+  }
+
+  createUser(data: any): Observable<any> {
+    return this.http.post(this.userUrl + '/create', data);
+  }
+
+  updatePrivacy(data: any): Observable<any> {
+    return this.http.put(this.userUrl + '/update-privacy', data);
+  }
+
+  updateCurrency(data: any): Observable<any> {
+    return this.http.put(this.userUrl + '/update-currency', data);
+  }
+}
