44d855ab17e629f8f27d88962643712faa95489e
federico <guidazzi.federico@gmail.com>
2024-08-03 15:50:19 +0200

    add new functionality to getAssetApi to retrieve data starting from a date

--- asset/src/main/java/com/example/asset/AssetApplication.java
+++ asset/src/main/java/com/example/asset/AssetApplication.java
@@ -2,8 +2,10 @@ package com.example.asset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AssetApplication {
 
     public static void main(String[] args) {
